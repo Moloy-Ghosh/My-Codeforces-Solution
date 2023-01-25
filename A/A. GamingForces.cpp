48ed6bcf28791821e1b8fc0fa666a6c0b9d38bf8
Moloy-Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-01-25 19:47:54 +0600

    Add files via upload

---
+++ A/A. GamingForces.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,c=0;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            if(arr[i]==1)c++;
+        }
+        cout<<(n-(c/2))<<endl;
+
+    }
+}
