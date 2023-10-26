46b3b57fe1c23e109abc90dcbefb3061b459beea
Moloy Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-10-26 17:29:14 +0600

    Add files via upload

---
+++ A. Candies and Two Sisters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        long long int n,res;
+        cin>>n;
+        if(n<3) cout<<'0'<<endl;
+        else if(n%2==0)cout<<(n/2)-1<<endl;
+        else cout<<(n/2)<<endl;
+    }
+}
